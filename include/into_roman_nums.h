30951685ce0e01008b9cb2a024d3958ac3e364b3
Dmitriy Ilyin <neytrino070902@yandex.ru>
2021-10-10 12:45:38 +0300

    some preparations for unit testing by gtest

--- include/into_roman_nums.h
+++ include/into_roman_nums.h
@@ -3,4 +3,4 @@
 
 char *into_roman_nums(int num, int *length);
 
-#endif //CPP_IZ_1_INTO_ROMAN_NUMS_H
+#endif // CPP_IZ_1_INTO_ROMAN_NUMS_H
---
+++ unit_tests/main.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "into_roman_nums.h"
+#include "result_codes.h"
+#include "io_functions.h"
+#include "print_errors.h"
+
+int main() {
+
+}
